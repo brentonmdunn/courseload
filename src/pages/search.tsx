d970ecb159a2fa874cd0a6a22c0a084758a97d79
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-03-09 22:33:31 -0800

    Basic serach functionality

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,15 +1,34 @@
 // import data from "../../scrapers/json/course_catalog.json";
 import data from "../../scrapers/json/data.json";
+import { useRef, useState } from "react";
 
 const search = () => {
+  const [filteredData, setFilteredData] = useState([]);
+
+  // const filteredItems = data.filter((item) => {
+  //   item.course_id.includes(query.toLowerCase());
+  // });
+
+  const handleFilter = (event: any) => {
+    const searchWord = event.target.value;
+    const newFilter: any = data.filter((value) => {
+      return value.course_id.includes(searchWord);
+    });
+    setFilteredData(newFilter);
+  };
+
   return (
     <div>
       <div>
-        <input type="text" />
+        <input onChange={handleFilter} type="search" />
       </div>
       <div>
-        {data.map((value, key) => {
-          return <div>{value.course_code}</div>;
+        {filteredData.map((value: any) => {
+          return (
+            <div>
+              {value.department} {value.course_code} {value.class_section_code}
+            </div>
+          );
         })}
       </div>
     </div>
