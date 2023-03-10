1c806876002183f7358750f5e423f4f625582d0c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2023-03-10 09:57:16 -0800

    Primitive search functionality

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -26,17 +26,47 @@ const search = () => {
       </div>
       <div className="box">
         {filteredData.map((value: any) => {
+          var colonIdxStart = value.start_time.indexOf(":");
+          var colonIdxEnd = value.end_time.indexOf(":");
+
+          var start_time, start_time_num;
+          var end_time, end_time_num;
+
+          if (parseInt(value.start_time.substring(0, colonIdxStart)) > 12) {
+            start_time_num =
+              parseInt(value.start_time.substring(0, colonIdxStart)) - 12;
+            start_time =
+              start_time_num.toString() +
+              value.start_time.substring(colonIdxStart) +
+              " PM";
+          } else {
+            start_time = value.start_time + " AM";
+          }
+
+          if (parseInt(value.end_time.substring(0, colonIdxEnd)) > 12) {
+            end_time_num =
+              parseInt(value.end_time.substring(0, colonIdxEnd)) - 12;
+            end_time =
+              end_time_num.toString() +
+              value.end_time.substring(colonIdxEnd) +
+              " PM";
+          } else {
+            end_time = value.end_time + " AM";
+          }
+
           return (
             <div className="query">
-              {value.department} {value.course_code} {value.class_section_code}
+              <p>
+                {value.department} {value.course_code}{" "}
+                {value.class_section_code} | {value.instructor}
+              </p>
               <ul>
-                <li>Instructor: {value.instructor}</li>
                 <li>
                   Location: {value.building} {value.room}
                 </li>
                 <li>Days: {value.days}</li>
                 <li>
-                  Time: {value.start_time} - {value.end_time}
+                  Time: {start_time} - {end_time}
                 </li>
               </ul>
             </div>
